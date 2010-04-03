3889b1d82a3b0d24942e52923f39ffa025086fdf
Troy Will <troydwill@gmail.com>
2010-04-03 14:33:30 -0700

    modified:   latest_tarball.csv

--- 2010_spring/csv/populate.sh
+++ 2010_spring/csv/populate.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 EDITOR=emacs
 
+${EDITOR} tmp/csv/webpages.csv webpages.csv
 ${EDITOR} tmp/csv/packages.csv packages.csv
 ${EDITOR} tmp/csv/latest_tarball.csv latest_tarball.csv
 ${EDITOR} tmp/csv/stowballs.csv stowballs.csv
-${EDITOR} tmp/csv/webpages.csv webpages.csv
 git add *.csv
 git commit -a
 git push origin master
-
