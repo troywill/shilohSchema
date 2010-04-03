d0ada0deb132d6e88ee884bcf1373cadf862eada
Troy Will <troydwill@gmail.com>
2010-04-03 13:48:53 -0700

    modified:   packages.csv

--- 2010_spring/csv/populate.sh
+++ 2010_spring/csv/populate.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 EDITOR=emacs
 
-${EDITOR} packages.csv tmp/csv/packages.csv
-${EDITOR} latest_tarball.csv tmp/csv/latest_tarball.csv
-${EDITOR} stowballs.csv tmp/csv/stowballs.csv
-${EDITOR} webpages.csv tmp/csv/webpages.csv
+${EDITOR} tmp/csv/packages.csv packages.csv
+${EDITOR} tmp/csv/latest_tarball.csv latest_tarball.csv
+${EDITOR} tmp/csv/stowballs.csv stowballs.csv
+${EDITOR} tmp/csv/webpages.csv webpages.csv
+git add *.csv
+git commit -a
+git push origin master
+
