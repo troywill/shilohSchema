2d62032a9c937b054b9cfb860f13a3ffab0dc04b
Troy Will <troydwill@gmail.com>
2010-04-12 00:37:24 -0700

    modified:   populate.sh

--- 2010_spring/csv/populate.sh
+++ 2010_spring/csv/populate.sh
@@ -2,8 +2,8 @@
 EDITOR=emacs
 STOWBALL_TOP_DIR='/home/troy/rcs/stowball'
 
-${EDITOR} tmp/csv/webpages.csv webpages.csv
 ${EDITOR} tmp/csv/packages.csv packages.csv
+${EDITOR} tmp/csv/webpages.csv webpages.csv
 ${EDITOR} tmp/csv/latest_tarball.csv latest_tarball.csv
 ${EDITOR} tmp/csv/stowballs.csv stowballs.csv
 git add *.csv
